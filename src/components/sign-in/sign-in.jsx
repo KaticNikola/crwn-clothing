769765f7b350c166916f3c688e701c0bfd8b6f78
Ilijaa <Ilijaa@users.noreply.github.com>
2019-08-13 11:53:23 +0200

    sign in plus btn

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-
+import FormInput from '../form-input/form-input';
+import CustomButton from '../custom-button/custom-button'
 
 import './sign-in.scss'
 
@@ -28,23 +29,23 @@ class SignIn extends React.Component{
 				<span>Sign in with your email and password</span>
 
 				<form onSubmit={this.handleSubmit}>
-					<input 
+					<FormInput 
 						type="email" 
 						name="email" 
 						value={ this.state.email } 
-						onChange={this.handleChange}
+						handleChange={this.handleChange}
+						label='email'
 						required/>
-					<label>Email</label>
 
-					<input 
+					<FormInput 
 						type="password" 
 						name="password" 
 						value={ this.state.password } 
-						onChange={this.handleChange}
+						handleChange={this.handleChange}
+						label='password'
 						required/>
-					<label>password</label>
 
-					<input type="submit" value="Submit Form"/>
+					<CustomButton type="submit">Sign In </CustomButton>
 				</form>
 			</div>
 		)
